3938a0aca73efc6c74aa0c4542fc94b3eb626292
Chili <planetchili@users.noreply.github.com>
2026-01-27 12:02:53 +0900

    remove legacy code from middleware

--- IntelPresentMon/PresentMonMiddleware/DynamicQuery.cpp
+++ IntelPresentMon/PresentMonMiddleware/DynamicQuery.cpp
@@ -6,6 +6,9 @@
 #include "../CommonUtilities/Hash.h"
 #include <unordered_map>
 
+using namespace pmon;
+using namespace mid;
+
 namespace
 {
 	struct TelemetryBindingKey_
@@ -38,76 +41,73 @@ namespace std
 	};
 }
 
-namespace pmon::mid::todo
+PM_DYNAMIC_QUERY::PM_DYNAMIC_QUERY(std::span<PM_QUERY_ELEMENT> qels, ipc::MiddlewareComms& comms)
 {
-	PM_DYNAMIC_QUERY::PM_DYNAMIC_QUERY(std::span<PM_QUERY_ELEMENT> qels, ipc::MiddlewareComms& comms)
-	{
-		const auto* introBase = comms.GetIntrospectionRoot();
-		pmapi::intro::Root introRoot{ introBase, [](const PM_INTROSPECTION_ROOT*) {} };
-		pmon::mid::ValidateQueryElements(qels, PM_METRIC_TYPE_DYNAMIC, introRoot, comms);
+	const auto* introBase = comms.GetIntrospectionRoot();
+	pmapi::intro::Root introRoot{ introBase, [](const PM_INTROSPECTION_ROOT*) {} };
+	pmon::mid::ValidateQueryElements(qels, PM_METRIC_TYPE_DYNAMIC, introRoot, comms);
 
-		std::unordered_map<TelemetryBindingKey_, RingMetricBinding*> telemetryBindings;
-		RingMetricBinding* frameBinding = nullptr;
+	std::unordered_map<TelemetryBindingKey_, RingMetricBinding*> telemetryBindings;
+	RingMetricBinding* frameBinding = nullptr;
 
-		size_t blobCursor = 0;
-		for (auto& qel : qels) {
-			RingMetricBinding* binding = nullptr;
-			if (qel.deviceId == ipc::kUniversalDeviceId) {
-				binding = frameBinding;
-				if (!binding) {
-					auto bindingPtr = MakeFrameMetricBinding(qel);
-					binding = bindingPtr.get();
-					frameBinding = bindingPtr.get();
-					ringMetricPtrs_.push_back(std::move(bindingPtr));
-				}
+	size_t blobCursor = 0;
+	for (auto& qel : qels) {
+		RingMetricBinding* binding = nullptr;
+		if (qel.deviceId == ipc::kUniversalDeviceId) {
+			binding = frameBinding;
+			if (!binding) {
+				auto bindingPtr = MakeFrameMetricBinding(qel);
+				binding = bindingPtr.get();
+				frameBinding = bindingPtr.get();
+				ringMetricPtrs_.push_back(std::move(bindingPtr));
+			}
+		}
+		else {
+			const TelemetryBindingKey_ key{
+				.deviceId = qel.deviceId,
+				.metric = qel.metric,
+				.arrayIndex = qel.arrayIndex,
+			};
+			if (auto it = telemetryBindings.find(key); it != telemetryBindings.end()) {
+				binding = it->second;
 			}
 			else {
-				const TelemetryBindingKey_ key{
-					.deviceId = qel.deviceId,
-					.metric = qel.metric,
-					.arrayIndex = qel.arrayIndex,
-				};
-				if (auto it = telemetryBindings.find(key); it != telemetryBindings.end()) {
-					binding = it->second;
-				}
-				else {
-					auto bindingPtr = MakeTelemetryRingMetricBinding(qel, introRoot);
-					binding = bindingPtr.get();
-					ringMetricPtrs_.push_back(std::move(bindingPtr));
-					telemetryBindings.emplace(key, binding);
-				}
+				auto bindingPtr = MakeTelemetryRingMetricBinding(qel, introRoot);
+				binding = bindingPtr.get();
+				ringMetricPtrs_.push_back(std::move(bindingPtr));
+				telemetryBindings.emplace(key, binding);
 			}
-
-			qel.dataOffset = blobCursor;
-			binding->AddMetricStat(qel, introRoot);
-			blobCursor = qel.dataOffset + qel.dataSize;
 		}
 
-		for (auto& binding : ringMetricPtrs_) {
-			binding->Finalize();
-		}
-
-		// make sure blob sizes are multiple of 16 bytes for blob array alignment purposes
-		blobSize_ = util::PadToAlignment(blobCursor, 16u);
+		qel.dataOffset = blobCursor;
+		binding->AddMetricStat(qel, introRoot);
+		blobCursor = qel.dataOffset + qel.dataSize;
 	}
 
-	size_t PM_DYNAMIC_QUERY::GetBlobSize() const
-	{
-		return blobSize_;
+	for (auto& binding : ringMetricPtrs_) {
+		binding->Finalize();
 	}
 
-	DynamicQueryWindow PM_DYNAMIC_QUERY::GenerateQueryWindow_(uint64_t nowTimestamp) const
-	{
-		return DynamicQueryWindow();
-	}
+	// make sure blob sizes are multiple of 16 bytes for blob array alignment purposes
+	blobSize_ = util::PadToAlignment(blobCursor, 16u);
+}
+
+size_t PM_DYNAMIC_QUERY::GetBlobSize() const
+{
+	return blobSize_;
+}
+
+DynamicQueryWindow PM_DYNAMIC_QUERY::GenerateQueryWindow_(uint64_t nowTimestamp) const
+{
+	return DynamicQueryWindow();
+}
 
 
-	void PM_DYNAMIC_QUERY::Poll(uint8_t* pBlobBase, ipc::MiddlewareComms& comms,
-		uint64_t nowTimestamp, FrameMetricsSource* frameSource) const
-	{
-		const auto window = GenerateQueryWindow_(nowTimestamp);
-		for (auto& pRing : ringMetricPtrs_) {
-			pRing->Poll(window, pBlobBase, comms, frameSource);
-		}
+void PM_DYNAMIC_QUERY::Poll(uint8_t* pBlobBase, ipc::MiddlewareComms& comms,
+	uint64_t nowTimestamp, FrameMetricsSource* frameSource) const
+{
+	const auto window = GenerateQueryWindow_(nowTimestamp);
+	for (auto& pRing : ringMetricPtrs_) {
+		pRing->Poll(window, pBlobBase, comms, frameSource);
 	}
 }
--- IntelPresentMon/PresentMonMiddleware/DynamicQuery.h
+++ IntelPresentMon/PresentMonMiddleware/DynamicQuery.h
@@ -8,60 +8,34 @@
 #include "RingMetricBinding.h"
 #include "DynamicQueryWindow.h"
 #include "../PresentMonAPI2/PresentMonAPI.h"
-#include "../ControlLib/CpuTelemetryInfo.h"
-#include "../ControlLib/PresentMonPowerTelemetry.h"
 
 
 namespace pmon::mid
 {
-	class Middleware;
 	class RingMetricBinding;
 }
 
 namespace pmon::ipc
 {
 	class MiddlewareComms;
-	class TelemetryMap;
 }
 
-namespace pmon::mid::todo
-{
-	struct PM_DYNAMIC_QUERY
-	{
-	public:
-		PM_DYNAMIC_QUERY(std::span<PM_QUERY_ELEMENT> qels, ipc::MiddlewareComms& comms);
-		size_t GetBlobSize() const;
-		void Poll(uint8_t* pBlobBase, ipc::MiddlewareComms& comms,
-			uint64_t nowTimestamp, FrameMetricsSource* frameSource) const;
-
-	private:
-		// functions
-		DynamicQueryWindow GenerateQueryWindow_(uint64_t nowTimestamp) const;
-		// data
-		std::vector<std::unique_ptr<RingMetricBinding>> ringMetricPtrs_;
-		size_t blobSize_;
-		// window parameters; these could theoretically be independent of query but current API couples them
-		double windowSizeMs_ = 0;
-		double metricOffsetMs_ = 0.;
-	};
-}
-
-// TODO: legacy: to be deleted
 struct PM_DYNAMIC_QUERY
 {
-	std::vector<PM_QUERY_ELEMENT> elements;
-	size_t GetBlobSize() const
-	{
-		return elements.back().dataOffset + elements.back().dataSize;
-	}
-	// Data used to track what should be accumulated
-	bool accumFpsData = false;
-	std::bitset<static_cast<size_t>(GpuTelemetryCapBits::gpu_telemetry_count)> accumGpuBits;
-	std::bitset<static_cast<size_t>(CpuTelemetryCapBits::cpu_telemetry_count)> accumCpuBits;
-	// Data used to calculate the requested metrics
-	double windowSizeMs = 0;
-	double metricOffsetMs = 0.;
-	size_t queryCacheSize = 0;
-	std::optional<uint32_t> cachedGpuInfoIndex;
+public:
+	PM_DYNAMIC_QUERY(std::span<PM_QUERY_ELEMENT> qels, pmon::ipc::MiddlewareComms& comms);
+	size_t GetBlobSize() const;
+	void Poll(uint8_t* pBlobBase, pmon::ipc::MiddlewareComms& comms,
+		uint64_t nowTimestamp, pmon::mid::FrameMetricsSource* frameSource) const;
+
+private:
+	// functions
+	pmon::mid::DynamicQueryWindow GenerateQueryWindow_(uint64_t nowTimestamp) const;
+	// data
+	std::vector<std::unique_ptr<pmon::mid::RingMetricBinding>> ringMetricPtrs_;
+	size_t blobSize_;
+	// window parameters; these could theoretically be independent of query but current API couples them
+	double windowSizeMs_ = 0;
+	double metricOffsetMs_ = 0.;
 };
 
